f9d7e3863d21c4033674035b54574d3a8f169e30
Rob Daniel <rob.daniel@uk.ibm.com>
2017-12-03 09:44:47 +0000

    can't connect to DB2

--- connect.php
+++ connect.php
@@ -3,19 +3,20 @@ $conn_parms = "connect_" . $_SERVER['environment'] . ".php";
 
 include "$conn_parms"; // include the connect_$country.php file (relevant connection file)
 
-$conn = db2_connect($system, $userid, $pwd); // connect to DB2 itself
+//$conn = db2_connect($system, $userid, $pwd); // connect to DB2 itself
+$conn=true;
 if ($conn) {
     // echo "Connection succeeded.<BR>";
     // if connection ok then connect to the relevant app schema (prefix is defined in the relevant connect_$country.php file
 
-    $Statement = "SET CURRENT SCHEMA='" . strtoupper($_SESSION['Db2Schema']) . "';";
-    $rs = db2_exec($conn, $Statement);
-    if (! $rs) {
-        echo "<br/>" . $Statement . "<br/>";
-        echo "<BR>" . db2_stmt_errormsg() . "<BR>";
-        echo "<BR>" . db2_stmt_error() . "<BR>";
-        exit("Set current schema failed");
-    }
+//     $Statement = "SET CURRENT SCHEMA='" . strtoupper($_SESSION['Db2Schema']) . "';";
+//     $rs = db2_exec($conn, $Statement);
+//     if (! $rs) {
+//         echo "<br/>" . $Statement . "<br/>";
+//         echo "<BR>" . db2_stmt_errormsg() . "<BR>";
+//         echo "<BR>" . db2_stmt_error() . "<BR>";
+//         exit("Set current schema failed");
+//     }
     $_SESSION['conn'] = $conn;
 } else {
     echo "<BR>" . "Connection Error Message :";
