cb9266e9212cbe357f6bf93fe3df557780cf3a15
Rob Daniel <rob.daniel@uk.ibm.com>
2017-12-03 15:41:34 +0000

    odbc ?

--- login.php
+++ login.php
@@ -30,34 +30,37 @@ if($auth->ensureAuthorized())
 
 echo "<h2>Try to connect</h2>";
 
-echo $_SESSION['VCAP_SERVICES'];
+$database = "BLUDB";        # Get these database details from
+$hostname = "dashdb-txn-flex-yp-dal09-541.services.dal.bluemix.net";  # the web console
+$user     = "bluadmin";   #
+$password = "NmE1MzFlNDI0NTI3";   #
+$port     = 50000;          #
+$ssl_port = 50001;          #
 
-$conn = db2_connect("dashdb-txn-flex-yp-dal09-541.services.dal.bluemix.net", "bluadmin", "NmE1MzFlNDI0NTI3"); // connect to DB2 itself
+# Build the connection string
+#
+$driver  = "DRIVER={IBM DB2 ODBC DRIVER};";
+$dsn     = "DATABASE=$database; " .
+"HOSTNAME=$hostname;" .
+"PORT=$port; " .
+"PROTOCOL=TCPIP; " .
+"UID=$user;" .
+"PWD=$password;";
+$ssl_dsn = "DATABASE=$database; " .
+"HOSTNAME=$hostname;" .
+"PORT=$ssl_port; " .
+"PROTOCOL=TCPIP; " .
+"UID=$user;" .
+"PWD=$password;" .
+"SECURITY=SSL;";
+$conn_string = $driver . $dsn;     # Non-SSL
+$conn_string = $driver . $ssl_dsn; # SSL
 
-if ($conn) {
-    echo "Connection succeeded.<BR>";
-    //if connection ok then connect to the relevant app schema (prefix is defined in the relevant connect_$country.php file
+# Connect
+#
+$conn = odbc_connect( $conn_string, "", "" );
 
-//         $Statement = "SET CURRENT SCHEMA='" . strtoupper($_SESSION['Db2Schema']) . "';";
-//         $rs = db2_exec($conn, $Statement);
-//         if (! $rs) {
-//             echo "<br/>" . $Statement . "<br/>";
-//             echo "<BR>" . db2_stmt_errormsg() . "<BR>";
-//             echo "<BR>" . db2_stmt_error() . "<BR>";
-//             exit("Set current schema failed");
-//         }
-    $_SESSION['conn'] = $conn;
-} else {
-    echo "<BR>" . "Connection Error Message :";
-    echo db2_conn_errormsg();
-    echo "<BR>" . "Connection Error Code :";
-    echo db2_conn_error();
-    echo "Connection failed.<BR>";
-    exit();
-}
-// db2_autocommit($conn, TRUE); // This is how it was on the Wintel Box - so the code has no/few commit points.
-
-?>
+var_dump($conn);
 
 
 ?>
