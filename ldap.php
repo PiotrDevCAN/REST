fe3a440d7772579953565e325c4b0bb960590ec4
Rob Daniel <rob.daniel@uk.ibm.com>
2017-12-03 10:29:34 +0000

    test ldap

---
+++ ldap.php
@@ -0,0 +1,47 @@
+<?php
+// connect, bind, and search for $user
+
+$w3php = array(
+
+    // enable debug only during testing
+    'debug' => TRUE,
+
+    // location of error documents
+    'error_doc' => $_SERVER['DOCUMENT_ROOT'] . "/" .  $site['prefix'] . '/error_doc/',
+
+    // enable or disable logging to syslog of auth attempts
+    'log_auth' => FALSE,
+
+    // ldap ssl connection url for authentication (iip authentication, bluepages)
+    'ldaps_host' => 'bluepages.ibm.com',
+
+    // attributes returned by default for authenticated users
+    'ldap_attr' => array(
+        'uid',
+        'mail',
+        'ismanager',
+        'dept',
+        'div',
+        'employeetype',
+        'ibmserialnumber',
+        'manager',
+        'cn',
+        'workloc'
+    ),
+
+    // 'ldap_attr' => array('uid', 'mail', 'dept', 'employeetype', 'ibmserialnumber', 'workloc'),
+    // base dn to use when doing iip authentication
+    'ldap_basedn' => 'ou=bluepages,o=ibm.com'
+);
+
+
+
+$ds = _ldaps_connect();
+var_dump($ds);
+
+$filter = "(&(mail=rob.daniel@uk.ibm.com)(objectclass=ibmPerson))";
+
+$sr = ldap_search($ds, $w3php['ldap_basedn'], $filter, $w3php['ldap_attr']);
+
+var_dump($sr);
+var_dump($ds);
