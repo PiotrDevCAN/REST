0e9f3f65f8eab3975e48c97e7d9fb0f6d5a58107
Rob Daniel <rob.daniel@uk.ibm.com>
2017-12-03 12:07:34 +0000

    More stuff for SSO

---
+++ cfg/openidconnect.inc.php
@@ -0,0 +1,9 @@
+<?php
+$config_openidconnect = new stdClass();
+$config_openidconnect->authorize_url = "https://prepiam.toronto.ca.ibm.com/idaas/oidc/endpoint/default/authorize";
+$config_openidconnect->token_url = "https://prepiam.toronto.ca.ibm.com/idaas/oidc/endpoint/default/token";
+$config_openidconnect->introspect_url = "https://prepiam.toronto.ca.ibm.com/idaas/oidc/endpoint/default/introspect ";
+$config_openidconnect->client_id = "MzY4MjNlYjMtOTI0NC00";
+$config_openidconnect->client_secret = "N2NkZTFjZTgtZmQ0Ny00";
+$config_openidconnect->redirect_url = "https://rest.w3ibm.mybluemix.net/auth/index.php";
+?>
