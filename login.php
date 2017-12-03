9e71b2afa934728fdaea72a4d14c6ac7d52d052b
Rob Daniel <rob.daniel@uk.ibm.com>
2017-12-03 15:48:59 +0000

    without odbc

--- login.php
+++ login.php
@@ -30,37 +30,37 @@ if($auth->ensureAuthorized())
 
 echo "<h2>Try to connect</h2>";
 
-$database = "BLUDB";        # Get these database details from
-$hostname = "dashdb-txn-flex-yp-dal09-541.services.dal.bluemix.net";  # the web console
-$user     = "bluadmin";   #
-$password = "NmE1MzFlNDI0NTI3";   #
-$port     = 50000;          #
-$ssl_port = 50001;          #
+// $database = "BLUDB";        # Get these database details from
+// $hostname = "dashdb-txn-flex-yp-dal09-541.services.dal.bluemix.net";  # the web console
+// $user     = "bluadmin";   #
+// $password = "NmE1MzFlNDI0NTI3";   #
+// $port     = 50000;          #
+// $ssl_port = 50001;          #
 
-# Build the connection string
-#
-$driver  = "DRIVER={IBM DB2 ODBC DRIVER};";
-$dsn     = "DATABASE=$database; " .
-"HOSTNAME=$hostname;" .
-"PORT=$port; " .
-"PROTOCOL=TCPIP; " .
-"UID=$user;" .
-"PWD=$password;";
-$ssl_dsn = "DATABASE=$database; " .
-"HOSTNAME=$hostname;" .
-"PORT=$ssl_port; " .
-"PROTOCOL=TCPIP; " .
-"UID=$user;" .
-"PWD=$password;" .
-"SECURITY=SSL;";
-$conn_string = $driver . $dsn;     # Non-SSL
-$conn_string = $driver . $ssl_dsn; # SSL
+// # Build the connection string
+// #
+// $driver  = "DRIVER={IBM DB2 ODBC DRIVER};";
+// $dsn     = "DATABASE=$database; " .
+// "HOSTNAME=$hostname;" .
+// "PORT=$port; " .
+// "PROTOCOL=TCPIP; " .
+// "UID=$user;" .
+// "PWD=$password;";
+// $ssl_dsn = "DATABASE=$database; " .
+// "HOSTNAME=$hostname;" .
+// "PORT=$ssl_port; " .
+// "PROTOCOL=TCPIP; " .
+// "UID=$user;" .
+// "PWD=$password;" .
+// "SECURITY=SSL;";
+// $conn_string = $driver . $dsn;     # Non-SSL
+// $conn_string = $driver . $ssl_dsn; # SSL
 
-# Connect
-#
-$conn = odbc_connect( $conn_string, "", "" );
+// # Connect
+// #
+// $conn = odbc_connect( $conn_string, "", "" );
 
-var_dump($conn);
+// var_dump($conn);
 
 
 
