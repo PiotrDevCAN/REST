a8017d6bc004e17dec5692a8638c0a82b778731a
Rob Daniel <rob.daniel@uk.ibm.com>
2017-12-03 14:57:20 +0000

    Try connect

--- login.php
+++ login.php
@@ -27,4 +27,37 @@ if($auth->ensureAuthorized())
     ?></pre><?php
 
 }
+
+echo "<h2>Try to connect</h2>";
+
+echo $_SESSION['VCAP_SERVICES'];
+
+$conn = db2_connect("dashdb-txn-flex-yp-dal09-541.services.dal.bluemix.net", "bluadmin", "NmE1MzFlNDI0NTI3"); // connect to DB2 itself
+
+if ($conn) {
+    echo "Connection succeeded.<BR>";
+    //if connection ok then connect to the relevant app schema (prefix is defined in the relevant connect_$country.php file
+
+//         $Statement = "SET CURRENT SCHEMA='" . strtoupper($_SESSION['Db2Schema']) . "';";
+//         $rs = db2_exec($conn, $Statement);
+//         if (! $rs) {
+//             echo "<br/>" . $Statement . "<br/>";
+//             echo "<BR>" . db2_stmt_errormsg() . "<BR>";
+//             echo "<BR>" . db2_stmt_error() . "<BR>";
+//             exit("Set current schema failed");
+//         }
+    $_SESSION['conn'] = $conn;
+} else {
+    echo "<BR>" . "Connection Error Message :";
+    echo db2_conn_errormsg();
+    echo "<BR>" . "Connection Error Code :";
+    echo db2_conn_error();
+    echo "Connection failed.<BR>";
+    exit();
+}
+// db2_autocommit($conn, TRUE); // This is how it was on the Wintel Box - so the code has no/few commit points.
+
+?>
+
+
 ?>
