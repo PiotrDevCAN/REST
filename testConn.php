26fa4ab89097cbeac989423af128aee873502258
Rob Daniel <rob.daniel@uk.ibm.com>
2017-12-04 14:58:24 +0000

    Teak 6

--- testConn.php
+++ testConn.php
@@ -3,21 +3,18 @@ ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(-1);
 
-var_dump($_SESSION);
-
 $sql = " SELECT * From REST.INFLIGHT_PROJECTS ";
 $rs = db2_exec($_SESSION['conn'],$sql);
 var_dump($rs);
 
 if($rs){
     $row = db2_fetch_assoc($rs);
-    var_dump($row);
+    echo "<br/>" . var_dump($row);
 }
 
 echo "<hr/>";
 
 $rs2 = db2_columns($_SESSION['conn'], null, $_SESSION['Db2Schema'], 'INFLIGHT_PROJECTS', '%');
-
 var_dump($rs2);
 
 if($rs2){
