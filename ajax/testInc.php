d5e7a919a465327caf3bd96f3875e1a41b3dd862
Rob Daniel <rob.daniel@uk.ibm.com>
2017-12-04 14:07:38 +0000

    improve siteheader for ajax

--- ajax/testInc.php
+++ ajax/testInc.php
@@ -1,6 +1,13 @@
 <?php
 include "here.inc";
-include "../here,inc";
+include "../here.inc";
 include "../vendor/phpoffice/phpexcel/Classes/PHPExcel/IOFactory.php";
 
 echo class_exists('PHPExcel_IOFactory');
+
+include "../../vendor/phpoffice/phpexcel/Classes/PHPExcel/IOFactory.php";
+
+echo class_exists('PHPExcel_IOFactory');
+
+echo __DIR__;
+
