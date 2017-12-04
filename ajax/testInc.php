1bed681d6298d5d0f0b6f39377d36eb64f2e79d9
Rob Daniel <rob.daniel@uk.ibm.com>
2017-12-04 14:16:54 +0000

    does this inc

--- ajax/testInc.php
+++ ajax/testInc.php
@@ -1,13 +1,6 @@
 <?php
 include "here.inc";
 include "../here.inc";
-include "../vendor/phpoffice/phpexcel/Classes/PHPExcel/IOFactory.php";
-
 echo class_exists('PHPExcel_IOFactory');
-
 include "../../vendor/phpoffice/phpexcel/Classes/PHPExcel/IOFactory.php";
-
 echo class_exists('PHPExcel_IOFactory');
-
-echo __DIR__;
-
