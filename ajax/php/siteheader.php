7771f79a4b278f6ac192c9dc78c8b213c3d77be3
Rob Daniel <rob.daniel@uk.ibm.com>
2017-12-04 16:23:05 +0000

    different symptoms - progress

--- ajax/php/siteheader.php
+++ ajax/php/siteheader.php
@@ -4,8 +4,10 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 date_default_timezone_set('UTC');
 set_include_path("./" . PATH_SEPARATOR . "../" . PATH_SEPARATOR . "../../" . PATH_SEPARATOR . "../../../" . PATH_SEPARATOR);
+
 include "vendor/autoload.php";
 include "splClassLoader.php";
+session_start();
 include "connect.php";
 
 ?>
