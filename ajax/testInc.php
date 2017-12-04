037ebc870a0582ec8f391180b608abbaa9e93aad
Rob Daniel <rob.daniel@uk.ibm.com>
2017-12-04 14:42:30 +0000

    Details

--- ajax/testInc.php
+++ ajax/testInc.php
@@ -2,5 +2,3 @@
 include "here.inc";
 include "../here.inc";
 echo class_exists('PHPExcel_IOFactory');
-include "../../vendor/phpoffice/phpexcel/Classes/PHPExcel/IOFactory.php";
-echo class_exists('PHPExcel_IOFactory');
--- testConn.php
+++ testConn.php
@@ -3,22 +3,25 @@ ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(-1);
 
-echo "<h2>Try to connect</h2>";
-include connect.php;
-
 var_dump($_SESSION);
-var_dump($expression);
 
 $sql = " SELECT COUNT(*) From REST.INFLIGHT_PROJECTS ";
 $rs = db2_exec($_SESSION['conn'],$sql);
 var_dump($rs);
 
+if($rs){
+    $row = db2_fetch_assoc($rs);
+    var_dump($row);
+}
 
 $rs = db2_columns($_SESSION['conn'], null, 'REST', 'INFLIGHT_PROJECTS', '%');
 
 var_dump($rs);
 
-
+if($rs){
+    while(($row = db2_fetch_assoc($rs))==true);
+    var_dump($row);
+}
 
 
 ?>
