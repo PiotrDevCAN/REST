16a66ef914f2d6b39e02fd69338cfeaa41ba2233
Rob Daniel <rob.daniel@uk.ibm.com>
2017-12-04 15:06:52 +0000

    Simples - No Logon

--- testConn.php
+++ testConn.php
@@ -3,15 +3,15 @@ ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
 error_reporting(-1);
 
-$sql = " SELECT * From REST.INFLIGHT_PROJECTS ";
-$rs = db2_exec($_SESSION['conn'],$sql);
-var_dump($rs);
+// $sql = " SELECT * From REST.INFLIGHT_PROJECTS ";
+// $rs = db2_exec($_SESSION['conn'],$sql);
+// var_dump($rs);
 
-if($rs){
-    while(($row = db2_fetch_assoc($rs))==true){
-    echo "<br/>" . var_dump($row);
-    }
-}
+// if($rs){
+//     while(($row = db2_fetch_assoc($rs))==true){
+//     echo "<br/>" . var_dump($row);
+//     }
+// }
 echo "<hr/>";
 
 $rs2 = db2_columns($_SESSION['conn'], null, $_SESSION['Db2Schema'], 'INFLIGHT_PROJECTS', '%');
@@ -25,16 +25,16 @@ if($rs2){
 
 echo "<hr/>";
 
-$rs3 = db2_tables($_SESSION['conn'], null, $_SESSION['Db2Schema']);
+// $rs3 = db2_tables($_SESSION['conn'], null, $_SESSION['Db2Schema']);
 
-var_dump($rs3);
+// var_dump($rs3);
 
-if($rs3){
-    while(($row = db2_fetch_assoc($rs3))==true){
-        echo "<br/>" . var_dump($row);
-    }
+// if($rs3){
+//     while(($row = db2_fetch_assoc($rs3))==true){
+//         echo "<br/>" . var_dump($row);
+//     }
 
-}
+// }
 
 
 ?>
