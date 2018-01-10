48df9463fd30b27bc93614a869ccc98a400a8fe6
Rob Daniel <rob.daniel@uk.ibm.com>
2018-01-10 16:28:42 +0000

    Don't like that.

--- rest/resourceRequestTable.php
+++ rest/resourceRequestTable.php
@@ -146,7 +146,7 @@ class resourceRequestTable extends DbTable
 
         $row['DESCRIPTION'] =
         "<button type='button' class='btn btn-default btn-xs deleteRecord' aria-label='Left Align' data-reference='" .$resourceReference . "' data-platform='" .trim($row['CURRENT_PLATFORM']) .  "' data-rfs='" .trim($row['RFS_ID']) . "' data-type='" . $resourceType . "' >
-            <span data-toggle='tooltip' title='Delete Resource' class='glyphicon glyphicon-trash ' aria-hidden='true' >&nbsp;</span>
+            <span data-toggle='tooltip' title='Delete Resource' class='glyphicon glyphicon-trash ' aria-hidden='true' ></span>
             </button>" . $description;
         $row['RESOURCE_NAME'] =
             "<button type='button' class='btn btn-default btn-xs editRecord' aria-label='Left Align' data-reference='" .$resourceReference . "' data-type='" .$resourceType . "' data-parent='" . $bwo_parent . "' >
