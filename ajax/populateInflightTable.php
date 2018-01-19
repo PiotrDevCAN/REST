90d3f4c3daa8f9b435b9a2930258b970fe5867b2
Rob Daniel <rob.daniel@uk.ibm.com>
2018-01-19 13:37:31 +0000

    Does it work in DEV?

--- ajax/populateInflightTable.php
+++ ajax/populateInflightTable.php
@@ -5,7 +5,10 @@ use rest\uploadLogTable;
 use rest\uploadLogRecord;
 set_time_limit(0);
 ob_start();
-$inflightTable = new inflightProjectsTable($_POST['tableName']);
+
+$tableName = isset($_POST['tableName']) ? $_POST['tableName'] : 'INFLIGHT_PROJECTS';
+
+$inflightTable = new inflightProjectsTable($tableName);
 
 $data = $inflightTable->returnAsArray();
 
@@ -15,21 +18,30 @@ $detailsOfLastLoad = $uploadLogTable->wasLastLoadSuccesssful();
 if($detailsOfLastLoad){
     $lastCompletedLogRecord = $detailsOfLastLoad['lastCompletedLogRecord'];
     $lastLoadAttempted      = $detailsOfLastLoad['lastLoadLogRecord'];
-    $lastLoadSuccessful     = $detailsOfLastLoad['Successful'];
 }
 
 
 $messages = ob_get_clean();
 
 $response = array('messages'=>$messages
-                 ,'lastLoad'=>array('tableName'=>trim($lastCompletedLogRecord->UPLOAD_TABLENAME),'fileName'=>trim($lastCompletedLogRecord->UPLOAD_FILENAME)
-                                                 ,'intranet'=>trim($lastCompletedLogRecord->UPLOAD_INTRANET),'timestamp'=>trim($lastCompletedLogRecord->UPLOAD_TIMESTAMP))
-                 ,'lastAttempted'=>array('tableName'=>trim($lastLoadAttempted->UPLOAD_TABLENAME),'fileName'=>trim($lastLoadAttempted->UPLOAD_FILENAME)
-                                        ,'intranet'=>trim($lastLoadAttempted->UPLOAD_INTRANET),'timestamp'=>trim($lastLoadAttempted->UPLOAD_TIMESTAMP ))
-                ,'lastloadSuccessful'=> $detailsOfLastLoad['Successful']
-                ,'data'=>$data
+                  ,'lastLoad'=>array('tableName'=>trim($lastCompletedLogRecord->UPLOAD_TABLENAME),'fileName'=>trim($lastCompletedLogRecord->UPLOAD_FILENAME)
+                                                  ,'intranet'=>trim($lastCompletedLogRecord->UPLOAD_INTRANET),'timestamp'=>trim($lastCompletedLogRecord->UPLOAD_TIMESTAMP))
+                  ,'lastAttempted'=>array('tableName'=>trim($lastLoadAttempted->UPLOAD_TABLENAME),'fileName'=>trim($lastLoadAttempted->UPLOAD_FILENAME)
+                                         ,'intranet'=>trim($lastLoadAttempted->UPLOAD_INTRANET),'timestamp'=>trim($lastLoadAttempted->UPLOAD_TIMESTAMP ))
+                 ,'lastloadSuccessful'=> $detailsOfLastLoad['Successful']
+//                 ,'data'=>$data
                 );
 
 
 ob_clean();
-echo json_encode($response);
+var_dump($response);
+echo "<hr/>";
+var_dump($data);
+echo "<hr/>";
+
+$dataJson = json_encode($data);
+var_dump($dataJson);
+
+$json =  json_encode($response);
+var_dump($json);
+die('the end');
