e288643a6168c0169fd1f32af69ddeea0b57ea25
Rob Daniel <rob.daniel@uk.ibm.com>
2018-01-29 14:42:36 +0000

    And the header needs sorting

--- rest/resourceRequestRecord.php
+++ rest/resourceRequestRecord.php
@@ -239,9 +239,18 @@ class resourceRequestRecord extends DbRecord
 
 
         $startDateObj = new \DateTime($startDate);
+        $day =  $startDateObj->format('d');
+        if($day > 28){
+            // We can't step through adding months if we start on 29th,30th or 31st.
+            $year = $startDateObj->format('Y');
+            $month = $startDateObj->format('m');
+            $startDateObj->setDate($year, $month, '28');
+        }
+
         $endDateObj = new \DateTime($endDate);
 
         if(empty($endDate)){
+            $endDateObj = \DateTime::createFromFormat('Y-m-d',$startDateObj->format('Y-m-d'));
             $endDateObj->modify("+6 months");
         }
 
