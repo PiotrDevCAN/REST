8356692e2d71c619b86092638f7a2933479cb990
Rob Daniel <rob.daniel@uk.ibm.com>
2018-02-02 15:11:57 +0000

    Increase Max HRS PER WEEK to 60

--- rest/resourceRequestRecord.php
+++ rest/resourceRequestRecord.php
@@ -136,7 +136,7 @@ class resourceRequestRecord extends DbRecord
    		<div class='form-group'>
    		<label for="HRS_PER_WEEK" class="col-md-2 control-label ceta-label-left" data-toggle="tooltip" data-placement="top" title="">Hrs per Week</label>
    		<div class="col-md-2">
-   		<input type='number' step='0.1' min=0 max=50 class="form-control" id="HRS_PER_WEEK" name="HRS_PER_WEEK" value="<?=$this->HRS_PER_WEEK?>" placeholder="Avg hrs/Week" <?=$notEditable?> >
+   		<input type='number' step='0.1' min=0 max=60 class="form-control" id="HRS_PER_WEEK" name="HRS_PER_WEEK" value="<?=$this->HRS_PER_WEEK?>" placeholder="Avg hrs/Week" <?=$notEditable?> >
    		<input id="originalHRS_PER_WEEK" name="originalHRS_PER_WEEK" value="<?=$this->HRS_PER_WEEK?>" type="hidden">
    		</div>
 
