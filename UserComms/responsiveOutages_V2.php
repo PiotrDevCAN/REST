fed36cf430c4f41d8b1ed414bdb71bba6a882065
R. A. G. Daniel <Rob.Daniel@uk.ibm.com>
2019-05-08 12:47:50 +0100

    Outages for Data Archive & V2 upgrade

--- UserComms/responsiveOutages_V2.php
+++ UserComms/responsiveOutages_V2.php
@@ -1,2 +1,4 @@
 <?php
 $plannedOutages->defineOutage(new DateTime('2018-01-02'),"Migrating Data to new tool");
+$plannedOutages->defineOutage(new DateTime('2019-05-10'),"Archiving old data");
+$plannedOutages->defineOutage(new DateTime('2019-05-17'),"Upgrade to V2");
