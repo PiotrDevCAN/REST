f8d2e0a8b3bc147ed7cf1b1e970bcc57d2350e2e
R. A. G. Daniel <Rob.Daniel@uk.ibm.com>
2019-10-16 15:39:21 +0100

    verify resp returned false

--- auth/index.php
+++ auth/index.php
@@ -9,12 +9,11 @@ include realpath(dirname(__FILE__))."/../class/include.php";
 $auth = new Auth();
 if($auth->verifyResponse($_GET))
 {
-    echo "<pre>";
-    var_dump($_GET);
-    die('here');
-
-
     header("Location: ".$_GET['state']);
 	exit();
+} else {
+    echo "<pre>";
+    var_dump($_GET);
+    die('verify response returned false');
 }
 ?>
