f2684de2fb61ca94f4a7718fbd16bed38b4eacc0
Robert Daniel <rob.daniel@uk.ibm.com>
2020-06-19 11:21:08 +0100

    Improve how we use SSO

--- cfg/openidconnect.inc.php
+++ cfg/openidconnect.inc.php
@@ -1,25 +1,33 @@
 <?php
 $config_openidconnect = new stdClass();
-$config_openidconnect->authorize_url  = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/authorize";
-$config_openidconnect->token_url      = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/token";
-$config_openidconnect->introspect_url = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/introspect";
 
-$config_openidconnect->client_id['rest']     = "MTYxNjY3ODYtODk5ZS00";
-$config_openidconnect->client_secret['rest'] = "MTA1Y2I1MGItNjAzZC00";
-$config_openidconnect->redirect_url['rest']  = "https://rest.w3ibm.mybluemix.net/auth/index.php";
+/*
+ * SSO Element of Config
+ *
+ */
 
-$config_openidconnect->client_id['rest_dev'] = "ZGE0NDYzMTctYmZhNS00";
-$config_openidconnect->client_secret['rest_dev'] = "ZWFhM2U5MTktNDk4NS00";
-$config_openidconnect->redirect_url['rest_dev'] = "https://restdev.w3ibm.mybluemix.net/auth/index.php";
+$config_openidconnect->client_id['staging']      = isset($_ENV['SSO_staging_client_id']) ?  $_ENV['SSO_staging_client_id'] : null;
+$config_openidconnect->client_secret['staging']  = isset($_ENV['SSO_staging_client_secret']) ? $_ENV['SSO_staging_client_secret'] : null;
 
-// $config_openidconnect->client_id['rest_xt'] = "NjRiYmY1NjYtZjhlYi00";
-// $config_openidconnect->client_secret['rest_xt'] = "YjZkMmUyMmMtNmViMS00";
-// $config_openidconnect->redirect_url['rest_xt'] = "https://rest-dev-x.w3ibm.mybluemix.net/auth/index.php";
+$config_openidconnect->authorize_url['staging']  = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/authorize";
+$config_openidconnect->token_url['staging']      = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/token";
+$config_openidconnect->introspect_url['staging'] = "https://w3id.alpha.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/introspect";
+
+$config_openidconnect->client_id['production']      = isset($_ENV['SSO_production_client_id']) ?  $_ENV['SSO_production_client_id'] : null;
+$config_openidconnect->client_secret['production']  = isset($_ENV['SSO_production_client_secret']) ? $_ENV['SSO_production_client_secret'] : null;
+
+$config_openidconnect->authorize_url['production']  = "https://w3id.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/authorize";
+$config_openidconnect->token_url['production']      = "https://w3id.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/token";
+$config_openidconnect->introspect_url['production'] = "https://w3id.sso.ibm.com/isam/oidc/endpoint/amapp-runtime-oidcidp/introspect";
+
+/*
+ * Application Instance of Config
+ *
+ */
+
+$config_openidconnect->redirect_url = "https://" . $_SERVER['HTTP_HOST'] . "/auth/index.php";
 
 
-$config_openidconnect->client_id['rest_xt'] = "ZTFkNzkzODktM2M3Ni00";
-$config_openidconnect->client_secret['rest_xt'] = "NDFmYTY1OWItYTFkMy00";
-$config_openidconnect->redirect_url['rest_xt'] = "https://rest-v21.w3ibm.mybluemix.net/auth/index.php";
 
 
 ?>
