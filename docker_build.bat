672124c17f0b18221fe8c1fbf1278f2d213e2fcb
Robert Daniel <rob.daniel@uk.ibm.com>
2020-06-19 12:43:13 +0100

    Tweaks to local docker build

--- docker_build.bat
+++ docker_build.bat
@@ -1,2 +1,2 @@
 docker build -t rest_2020 . --no-cache 
-docker run -dit -p 8080-9000:8080 --name rest_2020 -v C:/Users/RobDaniel/git/REST_bm:/var/www/html --env-file C:/Users/RobDaniel/git/REST_bm/dev_env.list rest_2020
+docker run -dit -p 8084:8080 --name rest_2020 -v C:/Users/RobDaniel/git/REST_bm:/var/www/html --env-file C:/Users/RobDaniel/git/REST_bm/dev_env.list rest_2020
