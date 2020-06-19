17962d138cdcd5119081d68200b978526778bc5c
Robert Daniel <rob.daniel@uk.ibm.com>
2020-06-19 11:32:08 +0100

    Changes for JWT non sticky sessions.

--- ajax/php/siteheader.php
+++ ajax/php/siteheader.php
@@ -1,13 +1,23 @@
 <?php
+use itdq\JwtSecureSession;
+
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 date_default_timezone_set('UTC');
 set_include_path("./" . PATH_SEPARATOR . "../" . PATH_SEPARATOR . "../../" . PATH_SEPARATOR . "../../../" . PATH_SEPARATOR);
 
-include "vendor/autoload.php";
-include "splClassLoader.php";
-session_start();
-include "connect.php";
+include ('vendor/autoload.php');
+include ('splClassLoader.php');
 
-?>
+$sessionConfig = (new \ByJG\Session\SessionConfig($_SERVER['SERVER_NAME']))
+->withSecret($_ENV['jwt_token']);
+
+$handler = new JwtSecureSession($sessionConfig);
+session_set_save_handler($handler, true);
+
+session_start();
+error_log(__FILE__ . "session:" . session_id());
+
+include "connect.php";
