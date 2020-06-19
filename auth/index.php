ded8f260d2f7f54c9a5e34dea34e7a1217ffa72f
Robert Daniel <rob.daniel@uk.ibm.com>
2020-06-19 13:32:21 +0100

    Bring in step with vbac

--- auth/index.php
+++ auth/index.php
@@ -1,8 +1,5 @@
 <?php
 //used to verify and process login
-ini_set('display_startup_errors',1);
-ini_set('display_errors',1);
-error_reporting(-1);
 
 include realpath(dirname(__FILE__))."/../class/include.php";
 $auth = new Auth();
@@ -10,9 +7,4 @@ if($auth->verifyResponse($_GET))
 {
     header("Location: ".$_GET['state']);
 	exit();
-} else {
-    echo "<pre>";
-    var_dump($_GET);
-    die('verify response returned false');
 }
-?>
