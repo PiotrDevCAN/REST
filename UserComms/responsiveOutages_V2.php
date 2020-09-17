44e2878b506ce86ff239102bc15c6c50c2f06db2
Robert Daniel <rob.daniel@uk.ibm.com>
2020-09-17 13:02:10 +0100

    Outage planned for 16th Oct

--- UserComms/responsiveOutages_V2.php
+++ UserComms/responsiveOutages_V2.php
@@ -3,4 +3,5 @@ $plannedOutages->defineOutage(new DateTime('2018-01-02'),"Migrating Data to new 
 $plannedOutages->defineOutage(new DateTime('2019-05-10'),"Archiving old data");
 $plannedOutages->defineOutage(new DateTime('2019-05-17'),"Upgrade to V2");
 $plannedOutages->defineOutage(new DateTime('2019-10-20'),"Planned Maintenance to IBM Dedicated Cloud");
-$plannedOutages->defineOutage(new DateTime('2020-10-20'),"When there are planned outages to the app or it's supporting infrastructure, the details will be found here");
+$plannedOutages->defineOutage(new DateTime('2020-10-16'),"V2.3 Iteration Release. Implement 'Pipeline', Diary feature,Auto-Close,...  ");
+$plannedOutages->defineOutage(new DateTime('2020-10-16'),"When there are planned outages to the app or it's supporting infrastructure, the details will be found here");
