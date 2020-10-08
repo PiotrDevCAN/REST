04a962cc637df73b45dcf415312b9855541a8f3d
Robert Daniel <rob.daniel@uk.ibm.com>
2020-10-08 07:23:21 +0100

    whops

--- rest/resourceRequestTable.php
+++ rest/resourceRequestTable.php
@@ -96,7 +96,7 @@ class resourceRequestTable extends DbTable
 
         $sql .=  " FROM " . $GLOBALS['Db2Schema'] . "." . $resourceRequestHoursTable;
         $sql .= "   WHERE  ( claim_month >= " . $startDateObj->format('m') . " and claim_year = " . $startDateObj->format('Y') . ")  " ;
-        $sql .= $startDateObj->format('Y') !==  $endDateObj->format('Y') ?  "    AND (claim_year >= " . $startDateObj->format('Y') . " and claim_year =< " . $endDateObj->format('Y') . " ) " : null;
+        $sql .= $startDateObj->format('Y') !==  $endDateObj->format('Y') ?  "    AND (claim_year >= " . $startDateObj->format('Y') . " and claim_year <= " . $endDateObj->format('Y') . " ) " : null;
         $sql .= "         AND (claim_month <= " . $endDateObj->format('m') . " and claim_year = " . $endDateObj->format('Y') . ")  " ;
         $sql .= " ) as resource_hours ";
         $sql .= " GROUP BY RESOURCE_REFERENCE ";
