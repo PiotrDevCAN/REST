6203e8187f6cbf4a2cbee6d2f93b188a76344a8c
Piotr <piotr.tajanowicz@ibm.com>
2021-05-19 13:01:26 +0200

    changes on lists

--- cdi_vbacPersonApi.php
+++ cdi_vbacPersonApi.php
@@ -40,24 +40,67 @@ if ($err) {
     $load = isset($_GET['load']) ? $_GET['load'] : false;
     if ($load) {
 
+        $loaded = new DateTime();
+        echo "<BR/>Database Load Started : " . $loaded->format('Y-m-d H:i:s');
+
         $success = true;
 
         $autoCommit = db2_autocommit($GLOBALS['conn']);
         db2_autocommit($GLOBALS['conn'],DB2_AUTOCOMMIT_OFF);   
         
         if (count($responseObj) > 0) {
-            foreach ($responseObj as $personEntry) {
-                $inactivePersonRecord->setFromArray($personEntry);
-                $db2result = $inactivePersonTable->insert($inactivePersonRecord);
-        
-                if(!$db2result){
-                    $success = false;
+            
+            $sql = "INSERT INTO " . $GLOBALS['Db2Schema'] . "." . allTables::$INACTIVE_PERSON . " ( EMAIL_ADDRESS, NOTES_ID, PES_STATUS )  Values ";
+                
+            foreach ($responseObj as $key => $personEntry) {
+                // -------------------------------------------------------------------
 
-                    echo db2_stmt_error();
-                    echo db2_stmt_errormsg();
-                } else {
-                    $loadCounter++;
+                // $inactivePersonRecord->setFromArray($personEntry);
+                // $db2result = $inactivePersonTable->insert($inactivePersonRecord);
+
+                // if(!$db2result){
+                //     $success = false;
+
+                //     echo db2_stmt_error();
+                //     echo db2_stmt_errormsg();
+                // } else {
+                //     $loadCounter++;
+                // } 
+
+                // -------------------------------------------------------------------
+
+                // $sql = "INSERT INTO " . $GLOBALS['Db2Schema'] . "." . allTables::$INACTIVE_PERSON . " ( EMAIL_ADDRESS, NOTES_ID, PES_STATUS ) ";
+                // $sql .= " Values ('" . db2_escape_string(trim($personEntry->EMAIL_ADDRESS)) . "','" . db2_escape_string($personEntry->NOTES_ID) . "','" . db2_escape_string($personEntry->PES_STATUS) . "' ) ";
+                
+                // $rs = DB2_EXEC ( $GLOBALS['conn'], $sql );
+                // if (! $rs) {
+                //     $success = false;
+                // } else {
+                //     $loadCounter++;
+                // }
+                
+                // Database Load Started : 2021-05-19 08:52:37
+                // Database Load Finished : 2021-05-19 08:59:06
+
+                // -------------------------------------------------------------------
+                if ($key > 0) {
+                    $sql .= " ,";    
                 }
+                $sql .= " ('" . db2_escape_string(trim($personEntry->EMAIL_ADDRESS)) . "','" . db2_escape_string($personEntry->NOTES_ID) . "','" . db2_escape_string($personEntry->PES_STATUS) . "' ) ";
+                
+                $loadCounter++;
+                
+                // -------------------------------------------------------------------                               
+            }
+            
+            // echo $sql;
+            // exit;
+
+            $rs = DB2_EXEC ( $GLOBALS['conn'], $sql );
+            if (! $rs) {
+                $success = false;
+            } else {
+                // $loadCounter++;
             }
         }
 
@@ -68,6 +111,9 @@ if ($err) {
         }
 
         db2_autocommit($GLOBALS['conn'],$autoCommit);
+
+        $loaded = new DateTime();
+        echo "<BR/>Database Load Finished : " . $loaded->format('Y-m-d H:i:s');
     }
 
     echo '<BR/><B>' . count($responseObj) . ' records read from VBAC api</B>';
