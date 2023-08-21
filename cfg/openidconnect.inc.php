9af0490df2dbe88eb17f3080c9841ffca4f41ffd
Piotr Tajanowicz <piotr.tajanowicz@kyndryl.com>
2023-08-21 13:37:23 +0200

    update day Clean Up

--- cfg/openidconnect.inc.php
+++ cfg/openidconnect.inc.php
@@ -9,12 +9,12 @@ $config_openidconnect = new stdClass();
 $config_openidconnect->client_id      = $_ENV['sso_client_id'];
 $config_openidconnect->client_secret  = $_ENV['sso_client_secret'];
 
-$config_openidconnect->authorize_url  = $_ENV['sso_authorize_url'];
-$config_openidconnect->token_url      = $_ENV['sso_token_url'];
-$config_openidconnect->introspect_url = $_ENV['sso_introspect_url'];
+$config_openidconnect->authorize_url  = $_ENV['sso_host'].'/authorize';
+$config_openidconnect->token_url      = $_ENV['sso_host'].'/token';
+$config_openidconnect->userinfo_url   = $_ENV['sso_host'].'/userinfo';
+$config_openidconnect->introspect_url = $_ENV['sso_host'].'/introspect';
 
 error_log('Authorising to:' . $config_openidconnect->authorize_url . " as (" . $config_openidconnect->client_id . ") ");
-
 
 /*
  * Application Instance of Config
@@ -22,8 +22,5 @@ error_log('Authorising to:' . $config_openidconnect->authorize_url . " as (" . $
  */
 
 $config_openidconnect->redirect_url = "https://" . $_SERVER['HTTP_HOST'] . "/auth/index.php";
-
-
-
 
 ?>
