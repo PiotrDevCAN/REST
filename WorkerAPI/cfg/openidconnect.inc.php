55fa6d8fc8f2070249f92265a90d645057359be9
Piotr Tajanowicz <piotr.tajanowicz@kyndryl.com>
2023-09-01 10:27:08 +0200

    Update

--- WorkerAPI/cfg/openidconnect.inc.php
+++ WorkerAPI/cfg/openidconnect.inc.php
@@ -10,11 +10,6 @@ $config_openidconnect->client_id      = $_ENV['worker_api_client_id'];
 $config_openidconnect->client_secret  = $_ENV['worker_api_client_secret'];
 $config_openidconnect->token_scope    = $_ENV['worker_api_token_scope'];
 
-// Token scope
-// DEV: api://51d4cf5d-b248-4e4f-b6dd-5897e73e247f/.default
-// TEST: api://61ed81e4-e465-4921-a2f9-dfbca0f0eae0/.default
-// PROD: api://b1bd8450-c0b1-46d9-99d2-52a55d58c8d2/.default
-
 $config_openidconnect->authorize_url  = $_ENV['worker_api_authority'].'/authorize';
 $config_openidconnect->token_url      = $_ENV['worker_api_authority'].'/token';
 $config_openidconnect->userinfo_url   = $_ENV['worker_api_authority'].'/userinfo';
