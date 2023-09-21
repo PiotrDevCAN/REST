08bac207c2493b2371ffb230d39f3a7f7073e00a
Piotr Tajanowicz <piotr.tajanowicz@kyndryl.com>
2023-09-21 13:44:46 +0200

    SiteHeaders settings update

--- api/php/siteheader.php
+++ api/php/siteheader.php
@@ -5,7 +5,12 @@ ob_start();
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
+
+ini_set('memory_limit', '512M');
+ini_set('max_execution_time', 360);
+
 error_reporting(E_ALL);
+
 date_default_timezone_set('UTC');
 set_include_path("./" . PATH_SEPARATOR . "../" . PATH_SEPARATOR . "../../" . PATH_SEPARATOR . "../../../" . PATH_SEPARATOR . "../itdq/" . PATH_SEPARATOR .  "../vbac/" );
 
@@ -20,6 +25,11 @@ if($_REQUEST['token']!= $_ENV['api_token']){
 include ('vendor/autoload.php');
 include ('splClassLoader.php');
 
+require_once("../php/errorHandlers.php");
+
+set_error_handler('myErrorHandler');
+register_shutdown_function('fatalErrorShutdownHandler');
+
 $sessionConfig = (new \ByJG\Session\SessionConfig($_SERVER['SERVER_NAME']))
 ->withTimeoutHours(24)
 ->withSecret($_ENV['jwt_token'])
@@ -28,8 +38,7 @@ $sessionConfig = (new \ByJG\Session\SessionConfig($_SERVER['SERVER_NAME']))
 $handler = new JwtSecureSession($sessionConfig);
 // session_set_save_handler($handler, true);
 
-// session_start();
-
+session_start();
 error_log(__FILE__ . "session:" . session_id());
 
 $GLOBALS['Db2Schema'] = strtoupper($_ENV['environment']);
